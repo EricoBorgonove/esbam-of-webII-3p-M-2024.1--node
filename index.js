adfdebf0207ac196f3cb53568675c992d7249564
ericoborgonove <borgonove@gmail.com>
2024-05-22 11:06:05 -0400

    arquivos iniciais

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require ('express')
+const path = require ('path')
+
+const app = express ()
+//const port = 3000
+const routes = require('./routes/routes.js')
+
+app.use (express.static('public'))
+app.use ('/', routes)
+
+//app.listen (port, ()=> {
+app.listen (process.env.PORT, ()=> {     
+      console.log (`App prodando na porta ${process.env.PORT}`)
+})
+
+// http://localhost:3000/ 
